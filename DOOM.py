99a495761d99f17b6f1cf256ae9c0f19591ef709
Daniel Garcia <danielga@andrew.cmu.edu>
2018-04-16 01:28:09 -0400

    Added collision with environment, and jumping!

--- DOOM.py
+++ DOOM.py
@@ -1,36 +1,73 @@
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import CollisionTraverser, CollisionHandlerPusher
+from panda3d.core import CollisionNode, CollisionSphere
+from panda3d.core import Point3
 import sys
 import math
 import os
 import random
 from direct.task.Task import Task
+from panda3d.core import CollisionTraverser, CollisionNode
+from panda3d.core import CollisionHandlerQueue, CollisionRay
 import time
+from panda3d.core import NodePath
+from panda3d.physics import *
 
 
 class MyApp(ShowBase):
 
     def __init__(self):
+        self.fall = 0
+
         ShowBase.__init__(self)
+        self.height = base.camera.getZ()
         # Load the environment model.
-        self.scene = self.loader.loadModel(
-            "/Users/danielgarcia/Docs/15-112-Term-Project/Test.egg")
+
         # Reparent the model to render.
-        self.scene.reparentTo(self.render)
         # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-10, -10, -10)
         base.disableMouse()
         self.createKeyControls()
 
         mySound = base.loader.loadSfx(
             "/Users/danielgarcia/Docs/15-112-Term-Project/At Doom's Gate.ogg")
-        # mySound.play()
+        mySound.play()
 
         self.keyMap = {"left": 0, "right": 0, "forward": 0, "backward": 0,
-                       "turn-left": 0, "turn-right": 0, "fast": 0}
+                       "turn-left": 0, "turn-right": 0, "fast": 0, "jump": 0}
         timer = 0.2
         taskMgr.doMethodLater(timer, self.move, "move")
+
+        # Initialize the collision traverser.
+        base.cTrav = CollisionTraverser()
+        base.cTrav.setRespectPrevTransform(True)
+
+        # Initialize the Pusher collision handler.
+        pusher = CollisionHandlerPusher()
+
+        # Create a collision node for this object.
+        cNode = CollisionNode('camera')
+        # Attach a collision sphere solid to the collision node.
+        cNode.addSolid(CollisionSphere(0, 0, 0, .75))
+        cNode.addSolid(CollisionSphere(0, 0, -10, .75))
+        # Attach the collision node to the object's model.
+        cameraC = base.camera.attachNewNode(cNode)
+        # Set the object's collision node to render as visible.
+        cameraC.show()
+
+        # Load another model.
+        self.scene = self.loader.loadModel(
+            "/Users/danielgarcia/Docs/15-112-Term-Project/Test.egg")
+        # Reparent the model to render.
+        self.scene.reparentTo(render)
+        # Set the position of the model in the scene.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(0, 0, 0)
+
+        # Add the Pusher collision handler to the collision traverser.
+        base.cTrav.addCollider(cameraC, pusher)
+        # Add the 'frowney' collision node to the Pusher collision handler.
+        pusher.addCollider(cameraC, base.camera, base.drive.node())
 
     def setKey(self, key, value):
         self.keyMap[key] = value
@@ -43,6 +80,7 @@ class MyApp(ShowBase):
         self.accept("a", self.setKey, ["backward", 1])
         self.accept("d", self.setKey, ["forward", 1])
         self.accept("shift", self.setKey, ["fast", 1])
+        self.accept("space-up", self.setKey, ["jump", 1])
 
         # directional movement - arrow up
         self.accept("w-up", self.setKey, ["left", 0])
@@ -66,15 +104,27 @@ class MyApp(ShowBase):
 
         if self.keyMap["forward"] > 0:
             (dx) = (1)
+            if self.keyMap["fast"] > 0:
+                dx *= 2
 
         elif self.keyMap["backward"] > 0:
             (dx) = (-1)
+            if self.keyMap["fast"] > 0:
+                dx *= 2
 
         if self.keyMap["left"] > 0:
             (dy) = (1)
+            if self.keyMap["fast"] > 0:
+                dy *= 2
 
         elif self.keyMap["right"] > 0:
             (dy) = (-1)
+            if self.keyMap["fast"] > 0:
+                dy *= 2
+
+        if self.keyMap["jump"] > 0:
+            self.fall = 5
+            self.keyMap["jump"] = 0
 
         if self.keyMap["turn-left"] > 0:
             (dh, dp, dr) = (1, 0, 0)
@@ -82,17 +132,18 @@ class MyApp(ShowBase):
         elif self.keyMap["turn-right"] > 0:
             (dh, dp, dr) = (-1, 0, 0)
 
+        if self.fall >= -5:
+            self.fall -= 0.5
+        dz += self.fall
+
         (newX, newY, newZ) = x + dx, y + dy, z + dz
         (newH, newP, newR) = h + dh, p + dp, r + dr
 
-        # update the positions & rotation
-        # base.camera.setX(newX)
-        # base.camera.setY(newY)
-        # base.camera.setZ(newZ)
-        if self.keyMap["fast"] > 0:
-            base.camera.setPos(base.camera, 2 * dx, 2 * dy, 2 * dz)
+        base.camera.setFluidPos(base.camera, dx, dy, dz)
+        if newZ == self.height:
+            self.fall = 0
         else:
-            base.camera.setPos(base.camera, dx, dy, dz)
+            self.height = newZ
 
         base.camera.setH(newH)
         base.camera.setP(newP)
