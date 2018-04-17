ea3a0b2956901152106952e14bb3f42d6dc2afb4
Daniel Garcia <danielga@andrew.cmu.edu>
2018-04-17 12:41:15 -0400

    Moved environment and Doomguy

--- DOOM.py
+++ DOOM.py
@@ -20,134 +20,69 @@ import Things
 class MyApp(ShowBase):
 
     def __init__(self):
-        self.fall = 0
 
         ShowBase.__init__(self)
-        self.height = base.camera.getZ()
         # Load the environment model.
 
         # Reparent the model to render.
         # Apply scale and position transforms on the model.
         base.disableMouse()
-        self.createKeyControls()
 
         mySound = base.loader.loadSfx(
             """/Users/danielgarcia/Docs/15-112-Term-Project/models/At Doom's Gate.ogg""")
         mySound.play()
 
-        self.keyMap = {"left": 0, "right": 0, "forward": 0, "backward": 0,
-                       "turn-left": 0, "turn-right": 0, "fast": 0, "jump": 0}
-        timer = 0.2
-        taskMgr.doMethodLater(timer, self.move, "move")
-
-        # Initialize the collision traverser.
+        # Initialize the Pusher collision handler.
+        pusher = CollisionHandlerPusher()
         base.cTrav = CollisionTraverser()
         base.cTrav.setRespectPrevTransform(True)
 
-        # Initialize the Pusher collision handler.
-        pusher = CollisionHandlerPusher()
+        timer = 0.2
+        self.doomGuy = Things.Doomguy(base, (0, 0, 0), (0, 0, 0),
+                                      base.camera, pusher, base.cTrav)
 
-        # Create a collision node for this object.
-        cNode = CollisionNode('camera')
-        # Attach a collision sphere solid to the collision node.
-        cNode.addSolid(CollisionSphere(2, 0, 0, .75))
-        cNode.addSolid(CollisionSphere(-2, 0, 0, .75))
-        cNode.addSolid(CollisionSphere(0, 2, 0, .75))
-        cNode.addSolid(CollisionSphere(0, -2, 0, .75))
-        cNode.addSolid(CollisionSphere(0, 0, -10, .75))
-        cNode.addSolid(CollisionSphere(0, 0, 2, .75))
-        # Attach the collision node to the object's model.
-        cameraC = base.camera.attachNewNode(cNode)
-        # Set the object's collision node to render as visible.
+        self.createKeyControls()
+        taskMgr.doMethodLater(timer, self.doomGuy.move, "move")
+
         self.scene = Things.Thing(base, 0, 0, 0, 0.25,
                                   "/Users/danielgarcia/Docs/15-112-Term-Project/models/Test.egg")
-
-        # Add the Pusher collision handler to the collision traverser.
-        base.cTrav.addCollider(cameraC, pusher)
-        # Add the 'camera' collision node to the Pusher collision handler.
-        pusher.addCollider(cameraC, base.camera, base.drive.node())
-
-    def setKey(self, key, value):
-        self.keyMap[key] = value
 
     def createKeyControls(self):
 
         # directional movement
-        self.accept("w", self.setKey, ["left", 1])
-        self.accept("s", self.setKey, ["right", 1])
-        self.accept("a", self.setKey, ["backward", 1])
-        self.accept("d", self.setKey, ["forward", 1])
-        self.accept("shift-w", self.setKey, ["left", 1])
-        self.accept("shift-s", self.setKey, ["right", 1])
-        self.accept("shift-a", self.setKey, ["backward", 1])
-        self.accept("shift-d", self.setKey, ["forward", 1])
-        self.accept("shift", self.setKey, ["fast", 1])
-        self.accept("space-up", self.setKey, ["jump", 1])
+        self.accept("w", self.doomGuy.setKey, ["left", 1])
+        self.accept("s", self.doomGuy.setKey, ["right", 1])
+        self.accept("a", self.doomGuy.setKey, ["backward", 1])
+        self.accept("d", self.doomGuy.setKey, ["forward", 1])
+        self.accept("shift-w", self.doomGuy.setKey, ["left", 1])
+        self.accept("shift-s", self.doomGuy.setKey, ["right", 1])
+        self.accept("shift-a", self.doomGuy.setKey, ["backward", 1])
+        self.accept("shift-d", self.doomGuy.setKey, ["forward", 1])
+        self.accept("shift", self.doomGuy.setKey, ["fast", 1])
+        self.accept("space-up", self.doomGuy.setKey, ["jump", 1])
 
         # directional movement - arrow up
-        self.accept("shift-w-up", self.setKey, ["left", 0])
-        self.accept("shift-s-up", self.setKey, ["right", 0])
-        self.accept("shift-a-up", self.setKey, ["backward", 0])
-        self.accept("shift-d-up", self.setKey, ["forward", 0])
-        self.accept("w-up", self.setKey, ["left", 0])
-        self.accept("s-up", self.setKey, ["right", 0])
-        self.accept("a-up", self.setKey, ["backward", 0])
-        self.accept("d-up", self.setKey, ["forward", 0])
-        self.accept("shift-up", self.setKey, ["fast", 0])
+        self.accept("shift-w-up", self.doomGuy.setKey, ["left", 0])
+        self.accept("shift-s-up", self.doomGuy.setKey, ["right", 0])
+        self.accept("shift-a-up", self.doomGuy.setKey, ["backward", 0])
+        self.accept("shift-d-up", self.doomGuy.setKey, ["forward", 0])
+        self.accept("w-up", self.doomGuy.setKey, ["left", 0])
+        self.accept("s-up", self.doomGuy.setKey, ["right", 0])
+        self.accept("a-up", self.doomGuy.setKey, ["backward", 0])
+        self.accept("d-up", self.doomGuy.setKey, ["forward", 0])
+        self.accept("shift-up", self.doomGuy.setKey, ["fast", 0])
 
         # turning movement
-        self.accept("shift-arrow_left", self.setKey, ["turn-left", 1])
-        self.accept("shift-arrow_right", self.setKey, ["turn-right", 1])
-        self.accept("shift-arrow_left-up", self.setKey, ["turn-left", 0])
-        self.accept("shift-arrow_right-up", self.setKey, ["turn-right", 0])
-        self.accept("arrow_left", self.setKey, ["turn-left", 1])
-        self.accept("arrow_right", self.setKey, ["turn-right", 1])
-        self.accept("arrow_left-up", self.setKey, ["turn-left", 0])
-        self.accept("arrow_right-up", self.setKey, ["turn-right", 0])
-
-    def move(self, task):
-        (x, y, z) = base.camera.getPos()
-        (h, p, r) = base.camera.getHpr()
-
-        (dx, dy, dz) = (0, 0, 0)
-        (dh, dp, dr) = (0, 0, 0)
-
-        dx = self.keyMap["forward"] - self.keyMap["backward"]
-
-        dy = self.keyMap["left"] - self.keyMap["right"]
-
-        if self.keyMap["jump"] > 0:
-            self.fall = 5
-            self.keyMap["jump"] = 0
-
-        if self.keyMap["turn-left"] > 0:
-            (dh, dp, dr) = (1, 0, 0)
-
-        elif self.keyMap["turn-right"] > 0:
-            (dh, dp, dr) = (-1, 0, 0)
-
-        if self.keyMap["fast"] > 0:
-            dx *= 2
-            dy *= 2
-
-        if self.fall >= -5:
-            self.fall -= 0.5
-        dz += self.fall
-
-        (newX, newY, newZ) = x + dx, y + dy, z + dz
-        (newH, newP, newR) = h + dh, p + dp, r + dr
-
-        base.camera.setFluidPos(base.camera, dx, dy, dz)
-        if newZ == self.height:
-            self.fall = 0
-        else:
-            self.height = newZ
-
-        base.camera.setH(newH)
-        base.camera.setP(newP)
-        base.camera.setR(newR)
-
-        return task.cont
+        self.accept("shift-arrow_left", self.doomGuy.setKey, ["turn-left", 1])
+        self.accept("shift-arrow_right", self.doomGuy.setKey, ["turn-right", 1])
+        self.accept("shift-arrow_left-up",
+                    self.doomGuy.setKey, ["turn-left", 0])
+        self.accept("shift-arrow_right-up",
+                    self.doomGuy.setKey, ["turn-right", 0])
+        self.accept("arrow_left", self.doomGuy.setKey, ["turn-left", 1])
+        self.accept("arrow_right", self.doomGuy.setKey, ["turn-right", 1])
+        self.accept("arrow_left-up", self.doomGuy.setKey, ["turn-left", 0])
+        self.accept("arrow_right-up", self.doomGuy.setKey, ["turn-right", 0])
 
 
 app = MyApp()
--- Things.py
+++ Things.py
@@ -16,6 +16,79 @@ from panda3d.physics import *
 from direct.gui.DirectGui import DirectFrame, DirectLabel
 
 
+class Doomguy(object):
+    def __init__(self, base, xyz, hpr, node, pusher, traverser):
+        self.x, self.y, self.z = xyz[0], xyz[1], xyz[2]
+        self.h, self.p, self.r = hpr[0], hpr[1], hpr[2]
+        self.node = node
+        self.keyMap = {"left": 0, "right": 0, "forward": 0, "backward": 0,
+                       "turn-left": 0, "turn-right": 0, "fast": 0, "jump": 0}
+        self.fall = 0
+        self.health = 100
+        # Create a collision node for this object.
+        cNode = CollisionNode('doomGuy')
+        # Attach a collision sphere solid to the collision node.
+        cNode.addSolid(CollisionSphere(2, 0, 0, .75))
+        cNode.addSolid(CollisionSphere(-2, 0, 0, .75))
+        cNode.addSolid(CollisionSphere(0, 2, 0, .75))
+        cNode.addSolid(CollisionSphere(0, -2, 0, .75))
+        cNode.addSolid(CollisionSphere(0, 0, -10, .75))
+        cNode.addSolid(CollisionSphere(0, 0, 2, .75))
+        # Attach the collision node to the object's model.
+        doomGuyC = self.node.attachNewNode(cNode)
+        # Add the Pusher collision handler to the collision traverser.
+        traverser.addCollider(doomGuyC, pusher)
+        # Add the 'camera' collision node to the Pusher collision handler.
+        pusher.addCollider(doomGuyC, self.node, base.drive.node())
+
+    def setKey(self, key, value):
+        self.keyMap[key] = value
+
+    def move(self, task):
+        (x, y, z) = self.node.getPos()
+        (h, p, r) = self.node.getHpr()
+
+        (dx, dy, dz) = (0, 0, 0)
+        (dh, dp, dr) = (0, 0, 0)
+
+        dx = self.keyMap["forward"] - self.keyMap["backward"]
+
+        dy = self.keyMap["left"] - self.keyMap["right"]
+
+        if self.keyMap["jump"] > 0:
+            self.fall = 5
+            self.keyMap["jump"] = 0
+
+        if self.keyMap["turn-left"] > 0:
+            (dh, dp, dr) = (1, 0, 0)
+
+        elif self.keyMap["turn-right"] > 0:
+            (dh, dp, dr) = (-1, 0, 0)
+
+        if self.keyMap["fast"] > 0:
+            dx *= 2
+            dy *= 2
+
+        if self.fall >= -5:
+            self.fall -= 0.5
+        dz += self.fall
+
+        (newX, newY, newZ) = x + dx, y + dy, z + dz
+        (newH, newP, newR) = h + dh, p + dp, r + dr
+
+        self.node.setFluidPos(self.node, dx, dy, dz)
+        if newZ == z:
+            self.fall = 0
+        else:
+            self.height = newZ
+
+        self.node.setH(newH)
+        self.node.setP(newP)
+        self.node.setR(newR)
+
+        return task.cont
+
+
 class Thing(ShowBase):
     def __init__(self, base, x, y, z, scale, modelPath):
         self.x = x
